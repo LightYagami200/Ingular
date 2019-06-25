889c9ea32cf0bdc5bd42ac4a291693066b224e2f
Abdul Sammad <abdulsammad200@gmail.com>
2019-06-25 15:54:47 +0500

    Code Formatting

--- commands/Moderation/mute.js
+++ commands/Moderation/mute.js
@@ -13,38 +13,46 @@ const { Command, Duration } = require('klasa');
 //   .add('muted', 'role'));
 
 module.exports = class extends Command {
+  constructor(...args) {
+    super(...args, {
+      permissionLevel: 6,
+      requiredPermissions: ['MANAGE_ROLES'],
+      runIn: ['text'],
+      description: 'Mutes a mentioned member.',
+      usage: '[when:time] <member:member> [reason:...string]',
+      usageDelim: ' '
+    });
+  }
 
-	constructor(...args) {
-		super(...args, {
-			permissionLevel: 6,
-			requiredPermissions: ['MANAGE_ROLES'],
-			runIn: ['text'],
-			description: 'Mutes a mentioned member.',
-			usage: '[when:time] <member:member> [reason:...string]',
-			usageDelim: ' '
-		});
-	}
+  async run(msg, [when, member, reason]) {
+    if (member.id === msg.author.id) throw 'Why would you mute yourself?';
+    if (member.id === this.client.user.id) throw 'Have I done something wrong?';
 
-	async run(msg, [when, member, reason]) {
-		if (member.id === msg.author.id) throw 'Why would you mute yourself?';
-		if (member.id === this.client.user.id) throw 'Have I done something wrong?';
+    if (member.roles.highest.position >= msg.member.roles.highest.position)
+      throw 'You cannot mute this user.';
 
-		if (member.roles.highest.position >= msg.member.roles.highest.position) throw 'You cannot mute this user.';
+    if (member.roles.has(msg.guild.settings.roles.muted))
+      throw 'The member is already muted.';
+    await member.roles.add(msg.guild.settings.roles.muted);
 
-		if (member.roles.has(msg.guild.settings.roles.muted)) throw 'The member is already muted.';
-		await member.roles.add(msg.guild.settings.roles.muted);
+    if (when) {
+      await this.client.schedule.create('unmute', when, {
+        data: {
+          guild: msg.guild.id,
+          user: member.id
+        }
+      });
+      return msg.sendMessage(
+        `${member.user.tag} got temporarily muted for ${Duration.toNow(when)}.${
+          reason ? ` With reason of: ${reason}` : ''
+        }`
+      );
+    }
 
-		if (when) {
-			await this.client.schedule.create('unmute', when, {
-				data: {
-					guild: msg.guild.id,
-					user: member.id
-				}
-			});
-			return msg.sendMessage(`${member.user.tag} got temporarily muted for ${Duration.toNow(when)}.${reason ? ` With reason of: ${reason}` : ''}`);
-		}
-
-		return msg.sendMessage(`${member.user.tag} got muted.${reason ? ` With reason of: ${reason}` : ''}`);
-	}
-
+    return msg.sendMessage(
+      `${member.user.tag} got muted.${
+        reason ? ` With reason of: ${reason}` : ''
+      }`
+    );
+  }
 };
--- commands/Moderation/unmute.js
+++ commands/Moderation/unmute.js
@@ -1,26 +1,29 @@
-// Copyright (c) 2017-2019 dirigeants. All rights reserved. MIT license.
 const { Command } = require('klasa');
 
 module.exports = class extends Command {
+  constructor(...args) {
+    super(...args, {
+      permissionLevel: 6,
+      requiredPermissions: ['MANAGE_ROLES'],
+      runIn: ['text'],
+      description: 'Unmutes a mentioned user.',
+      usage: '<member:member> [reason:...string]',
+      usageDelim: ' '
+    });
+  }
 
-	constructor(...args) {
-		super(...args, {
-			permissionLevel: 6,
-			requiredPermissions: ['MANAGE_ROLES'],
-			runIn: ['text'],
-			description: 'Unmutes a mentioned user.',
-			usage: '<member:member> [reason:...string]',
-			usageDelim: ' '
-		});
-	}
+  async run(msg, [member, reason]) {
+    if (member.roles.highest.position >= msg.member.roles.highest.position)
+      throw 'You cannot unmute this user.';
+    if (!member.roles.has(msg.guild.settings.roles.muted))
+      throw 'This user is not muted.';
 
-	async run(msg, [member, reason]) {
-		if (member.roles.highest.position >= msg.member.roles.highest.position) throw 'You cannot unmute this user.';
-		if (!member.roles.has(msg.guild.settings.roles.muted)) throw 'This user is not muted.';
+    await member.roles.remove(msg.guild.settings.roles.muted);
 
-		await member.roles.remove(msg.guild.settings.roles.muted);
-
-		return msg.sendMessage(`${member.user.tag} was unmuted.${reason ? ` With reason of: ${reason}` : ''}`);
-	}
-
+    return msg.sendMessage(
+      `${member.user.tag} was unmuted.${
+        reason ? ` With reason of: ${reason}` : ''
+      }`
+    );
+  }
 };
